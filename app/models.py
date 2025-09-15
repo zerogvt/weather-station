44baa9a508f67522e9a38ceef46dec11875cb56a
zerogvt <gkoumplias@gmail.com>
2025-09-15 12:12:29 +0300

    full schema

--- app/models.py
+++ app/models.py
@@ -1,18 +1,20 @@
+import enum
 from uuid import uuid4, UUID
 from dataclasses import dataclass
 from sqlalchemy.types import TIMESTAMP
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from datetime import date
 
 
 @dataclass
 class Measurement(db.Model):
-    #id: so.Mapped[UUID] = so.mapped_column(sa.UUID,primary_key=True,default=uuid4)
+    __tablename__ = "measurement"
     measurement_id: so.Mapped[str] = so.mapped_column(sa.String(64))
     station: so.Mapped[str] = so.mapped_column(sa.String(64), index=True)
     sensor_id: so.Mapped[str] = so.mapped_column(sa.String(64),
-                                                 primary_key=True, index=True)
+                                                 primary_key=True)
     date: so.Mapped[TIMESTAMP] = so.mapped_column(sa.TIMESTAMP(timezone=True),
                                                   primary_key=True)
     category: so.Mapped[str] = so.mapped_column(sa.String(64))
@@ -24,8 +26,9 @@ class Measurement(db.Model):
 
 @dataclass
 class Station(db.Model):
+    __tablename__ = "station"
     code: so.Mapped[str] = so.mapped_column(sa.String(64),
-                                            primary_key=True, index=True)
+                                            primary_key=True)
     city: so.Mapped[str] = so.mapped_column(sa.String(64), index=True)
     latitude: so.Mapped[float] = so.mapped_column(sa.Float)
     longtitude: so.Mapped[float] = so.mapped_column(sa.Float)
@@ -33,3 +36,26 @@ class Station(db.Model):
  
     def __repr__(self):
         return f"<City {self.code} {self.city}>"
+
+@dataclass
+class Sensor(db.Model):
+    __tablename__ = "sensor"
+    id: so.Mapped[str] = so.mapped_column(sa.String(64),
+                                                 primary_key=True)
+    station_code: so.Mapped[str] = so.mapped_column(sa.ForeignKey("station.code"), index=True)
+    category: so.Mapped[str] = so.mapped_column(sa.String(64))
+    
+    def __repr__(self):
+        return f"<Sensor {self.id} {self.station_code} {self.category}>"
+
+@dataclass
+class Forecast(db.Model):
+    __tablename__ = "forecast"
+    forecast_date: so.Mapped[date] = so.mapped_column(sa.Date, primary_key=True)
+    city: so.Mapped[str] = so.mapped_column(sa.String(64), primary_key=True)
+    temperature: so.Mapped[int] = so.mapped_column(sa.Integer)
+    humidity: so.Mapped[int] = so.mapped_column(sa.Integer)
+    wind: so.Mapped[int] = so.mapped_column(sa.Integer)
+ 
+    def __repr__(self):
+        return f"<Forecast {self.date} {self.city} {self.temperature} {self.humidity} {self.wind}>"
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, async_session
 from app.utils import flatten_data
-from app.models import Measurement
+from app.models import Measurement, Forecast, Station, Sensor
 from flask import jsonify, request
 from sqlalchemy.sql import select
 from sqlalchemy.exc import IntegrityError
@@ -11,7 +11,6 @@ from sqlalchemy.exc import IntegrityError
 def index():
     return "Hello, World!"
 
-
 @app.route('/measurements')
 async def get_data():
     async with async_session() as session:
@@ -19,6 +18,7 @@ async def get_data():
         result = await session.execute(stmt)
         data = result.fetchall()
         return jsonify([tuple(row) for row in data])
+
 
 @app.route('/ingest', methods=['POST'])
 async def ingest_data():
@@ -31,6 +31,46 @@ async def ingest_data():
                 session.add(M)
                 return jsonify(M), 201
     except TypeError as e:
-        return {"error": f"Malformed measurement: {e}"},  400
+        return {"error": f"Malformed data: {e}"},  400
+    except IntegrityError as e:
+        return {"error": f"Integrity Error: {e}"},  400
+
+
+@app.route('/forecast', methods=['POST'])
+async def add_forecast():
+    try:
+        F = Forecast(**request.json)
+        async with async_session() as session:
+            async with session.begin():
+                session.add(F)
+                return jsonify(F), 201
+    except TypeError as e:
+        return {"error": f"Malformed data: {e}"},  400
+    except IntegrityError as e:
+        return {"error": f"Integrity Error: {e}"},  400
+
+@app.route('/station', methods=['POST'])
+async def add_station():
+    try:
+        S = Station(**request.json)
+        async with async_session() as session:
+            async with session.begin():
+                session.add(S)
+                return jsonify(S), 201
+    except TypeError as e:
+        return {"error": f"Malformed data: {e}"},  400
+    except IntegrityError as e:
+        return {"error": f"Integrity Error: {e}"},  400
+
+@app.route('/sensor', methods=['POST'])
+async def add_sensor():
+    try:
+        S = Sensor(**request.json)
+        async with async_session() as session:
+            async with session.begin():
+                session.add(S)
+                return jsonify(S), 201
+    except TypeError as e:
+        return {"error": f"Malformed data: {e}"},  400
     except IntegrityError as e:
         return {"error": f"Integrity Error: {e}"},  400
